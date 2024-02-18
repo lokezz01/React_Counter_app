fbb5915bfdcc84acb8f0657f99fb950f6dec0100
unknown <lokeshbala61@gmail.com>
2024-02-18 16:46:09 +0530

    Counter app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Content from "./Content";
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+        
+        <Content/>
+        
+    </div>
+  );
+}
+
+export default App;
